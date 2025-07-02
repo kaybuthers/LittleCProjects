5deaf84a5826d2873ac976df5cffa634325151d3
Erika <79380106+kaybuthers@users.noreply.github.com>
2025-07-02 19:15:13 -0300

    Create xadrez.c

---
+++ xadrez.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(){
+    // Declaração de variáveis
+    char peca;
+    int casas, i = 0;
+
+    // Inicio do jogo de Xadrez
+    printf("Bem vindo ao Jogo de Xadrez!\n");
+    printf("--------------------------\n");
+    printf("Neste jogo, você irá movimentar três das peças do xadrez: Rainha, Torre e Bispo.\n");
+    printf("Escolha a peça e diga quantas casas ela irá andar.\n");
+    printf("Bispo e Torre estarão se movimentando apenas para direita e a Rainha para a esquerda por enquanto.\n");
+    printf("--------------------------\n");    
+    
+    // Solicitar informações para o jogador
+    printf("Escolha a peça que irá movimentar:\n");
+    printf("Digite B para Bispo, R para Rainha e T para Torre:\n");
+    scanf("%c", &peca);
+    printf("Escolha quantas casas andar:\n");
+    scanf("%d", &casas);
+
+    switch (peca)
+    {
+    case 'B':
+        // Loop para o Bispo
+        do{
+            printf("O Bispo se movimentou para Cima e para Direita.\n"); // Imprime direção do movimento
+            i++; // Incremento do contador
+        } while (i < casas);
+        break;
+    case 'R':
+        // Loop para a Rainha
+        while (i < casas){
+            printf("A Rainha se movimentou para Esquerda.\n"); // Imprime direção do movimento
+            i++; // Incremento do contador
+        }
+    case 'T':
+        // Loop para Torre
+        for(i; i < casas; i++){
+            printf("A Torre se movimentou para Direita.\n"); // Imprime direção do movimento
+        }
+        break;
+    default:
+        break;
+    }
+    
+    printf("--------------Fim de Jogo------------\n");
+
+    return 0;
+}
